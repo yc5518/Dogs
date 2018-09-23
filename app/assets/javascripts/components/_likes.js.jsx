50274c919db49c72078699782aa736e3a9ce9c49
Chao Yang <yc5518@hotmail.com>
2018-09-24 01:14:40 +0800

    disabled real 'like' feature, just pop up login message

--- app/assets/javascripts/components/_likes.js.jsx
+++ app/assets/javascripts/components/_likes.js.jsx
@@ -9,19 +9,21 @@ class Like extends React.Component {
     }
 
     handleOnClickLikes(){
-        let body = JSON.stringify({breed: {name: this.props.breed.name, img_url:this.props.breed.img_url, likes:this.props.breed.likes + 1} })
-        fetch('http://localhost:3000/api/v1/breeds', {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: body,
-        }).then((response) => {return response.json()})
-            .then((breed)=>{
-                this.setState({
-                    likes:breed.likes
-                })
-            })
+        // let body = JSON.stringify({breed: {name: this.props.breed.name, img_url:this.props.breed.img_url, likes:this.props.breed.likes + 1} })
+        // fetch('http://localhost:3000/api/v1/breeds', {
+        //     method: 'PUT',
+        //     headers: {
+        //         'Content-Type': 'application/json'
+        //     },
+        //     body: body,
+        // }).then((response) => {return response.json()})
+        //     .then((breed)=>{
+        //         this.setState({
+        //             likes:breed.likes
+        //         })
+        //     })
+
+        alert('Please login to like this breed!')
     }
 
     render() {
